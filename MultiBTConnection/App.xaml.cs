d8b0bb5be8753e95d96c113d8060a2962fdc29b3
Prabin Rag TK <prabinrag2340@gmail.com>
2024-12-06 23:53:36 +0400

    Add project files.

---
+++ MultiBTConnection/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MultiBTConnection
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ MultiBTConnection/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MultiBTConnection
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
